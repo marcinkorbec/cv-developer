f6e99bc0a8ad33df9374a42662b298fd8087a17f
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-06-25 18:24:14 +0200

    Refactor code.

---
+++ src/utils/personal-data.js
@@ -0,0 +1,19 @@
+export const blueHeaderData = {
+	photo: "https://imagizer.imageshack.com/img923/2025/VB8N59.png",
+	name: "Marcin Jacek Korbecki",
+	title: "Junior JavaScript & Web Developer",
+	cvPdfUrl: "https://sofarsolarpoland.pl/wp-content/uploads/2020/12/3.3-12ktl-x.pdf",
+}
+
+export const personalDesrciptionData = {
+	aboutMe: "Cześć! Jestem Marcin. Jestem samoukiem - pasjonatem programowania i Web Designu. Od prawie trzech lat się tym zajmuję amatorsko i ciągle się uczę. Posiadam juniorskie umiejętności z zakresu JavaScriptu, TypeScriptu, Baz Danych oraz Reacta. Znam też Wordpressa i od niego zaczynałem swoją przygodę, najpierw robiąc dla siebie jakieś stronki zarabiające np na reklamach, a z czasem wykonywałem dorywczo zlecenia freelancerskie w postaci jakichś prostych Squeeze-page'y, stron wizytówek, po blogi na Wordpress. Robiłem też szablony do WP szablony front-endowe. Od pewnego czasu dąże do profesjonalizacji uczestnicze w kursach i chciałbym pracować w zawodzie programisty na pełen etat. Na swoim blogu, napisałem więcej, o tym skąd się u mnie wzieło w głowie programowanie i dlaczego dopiero teraz.",
+	aboutMeUrl: "http://www.webmarcin.pl/o-tym-jak-zapragnalem-byc-programista/"
+}
+
+export const detailsData = {
+	age: "34",
+	email: "poczta@webmarcin.pl",
+	phone: "+48-501-360-355",
+	address: "Zabratówka 119c",
+	postCode: "36-017"
+}
