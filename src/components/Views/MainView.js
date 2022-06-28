0cfb31b1d20c1a66b49901d64f38d6a9e0b17c3a
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-06-28 22:16:58 +0200

    Refactor code.

--- src/components/Views/MainView.js
+++ src/components/Views/MainView.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {Header} from "../Header/Header";
 import {PageContent} from "../PageContent/PageContent";
 import {Footer} from "../Footer/Footer";
-import {CVData} from "../../utils/personal-data";
+import {cvData, CVData} from "../../utils/personal-data";
 
 export const MainView = () => {
+
+	// const headerData = props.find
 
   return (
     <div id="root">
