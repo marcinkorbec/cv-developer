a12fb145953a97ada8e549063ddd32604d273835
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-07-04 18:52:58 +0200

    Finalization of the socialmedia module.

--- src/components/SocialMedia/SocialMedia.js
+++ src/components/SocialMedia/SocialMedia.js
@@ -1,34 +1,52 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 export const SocialMedia = () => {
-	 const socialMediaData = [
-		 {
 
-		 }
-	 ]
+	const [cvData, setCvData] = useState(null);
 
-	return (
-  <ul className="nav justify-content-center">
-    <li className="nav-item"><a className="nav-link" href="https://www.linkedin.com/in/marcin-korbecki-9021661a5/"
-                                title="LinkedIn" target="_blank"><i className="fab fa-linkedin"/><span
-      className="menu-title sr-only">Linked In</span></a>
-    </li>
+	useEffect(() => {
+		(async () => {
+			const res = await fetch('http://localhost:3021/get-items');
+			const data = await res.json();
+			const tab = await data.filter(data => data.index > 4 && data.index < 9);
+			console.log(tab)
+			setCvData(tab);
+		})();
+	}, []);
 
-    <li className="nav-item"><a className="nav-link" href="https://webmarcin.pl"
-                                title="Mój Blog" target="_blank"><i className="fab fa-blogger"/><span
-      className="menu-title sr-only">Mój Blog</span></a>
-    </li>
-    <li className="nav-item"><a className="nav-link" href="https://www.facebook.com/uczszsiewkoncu"
-                                title="Facebook" target="_blank"><i className="fab fa-facebook"/><span
-      className="menu-title sr-only">Facebook</span></a>
-    </li>
-    <li className="nav-item"><a className="nav-link" href="https://www.instagram.com/m.j.korbecki/"
-                                title="Instagram" target="_blank"><i className="fab fa-instagram"/><span
-      className="menu-title sr-only">Instagram</span></a>
-    </li>
-    <li className="nav-item"><a className="nav-link" href="https://github.com/marcinkorbec"
-                                title="Github" target="_blank"><i className="fab fa-github"/><span
-      className="menu-title sr-only">Github</span></a>
-    </li>
-  </ul>
-)}
+	console.log(cvData)
+
+
+	if (cvData === null) {
+		return (
+			<div className="lds-heart">
+				<div></div>
+			</div>
+		)
+	} else {
+		return (
+			<ul className="nav justify-content-center">
+				{
+					cvData.map(cvItem => (
+						<li className="nav-item"
+								key={cvItem._id}>
+							<a className="nav-link"
+								 href={cvItem.link}
+								 title={cvItem.name}
+								 target="_blank"
+							>
+								<i
+									className={cvItem.faIcon}
+								/>
+								<span
+									className="menu-title sr-only">{cvItem.name}
+							</span>
+							</a>
+						</li>
+					))
+				}
+			</ul>
+		)
+	}
+
+}
