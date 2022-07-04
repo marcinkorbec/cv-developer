21bd79b3e889d4038c60858c6018cedea8351394
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-07-04 20:11:52 +0200

    Refactor Details.js.

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,32 +1,31 @@
 import React from "react";
-import {detailsData} from "../../utils/personal-data";
 
-export const Details = () => (
+export const Details = (props) => (
   <div className="col-md-5 offset-md-1">
     <div className="row mt-2">
       <div className="col-sm-4">
         <div className="pb-1"><strong>Wiek:</strong></div>
       </div>
       <div className="col-sm-8">
-        <div className="pb-1 text-secondary">{detailsData.age}</div>
+        <div className="pb-1 text-secondary">{props[0].age}</div>
       </div>
       <div className="col-sm-4">
         <div className="pb-1"><strong>Email:</strong></div>
       </div>
       <div className="col-sm-8">
-        <div className="pb-1 text-secondary">{detailsData.email}</div>
+        <div className="pb-1 text-secondary">{props[0].email}</div>
       </div>
       <div className="col-sm-4">
         <div className="pb-1"><strong>Telefon:</strong></div>
       </div>
       <div className="col-sm-8">
-        <div className="pb-1 text-secondary">{detailsData.phone}</div>
+        <div className="pb-1 text-secondary">{props[0].phone}</div>
       </div>
       <div className="col-sm-4">
         <div className="pb-1"><strong>Adres:</strong></div>
       </div>
       <div className="col-sm-8">
-        <div className="pb-1 text-secondary">{detailsData.address}, {detailsData.postCode}</div>
+        <div className="pb-1 text-secondary">{props[0].address}, {props[0].postCode}</div>
       </div>
     </div>
   </div>
