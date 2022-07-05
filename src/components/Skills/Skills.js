b4d83abb4ecd0de276c22ffdb75d00a17f1eaa3c
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-07-05 16:35:04 +0200

    Refactor Skills.js

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,55 +1,68 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
-export const Skills = () => (
-	<div className="skills-section px-3 px-lg-4">
-		<h2 className="h3 mb-3">Umiejętności</h2>
-		<div className="row">
-			<div className="col-md-6">
-				<div className="mb-2"><span>HTML</span>
-					<div className="progress my-1">
-						<div className="progress-bar bg-primary" role="progressbar" data-aos="zoom-in-right"
-								 data-aos-delay="100" data-aos-anchor=".skills-section" style={{width: '90%'}} aria-valuenow="90"
-								 aria-valuemin="0" aria-valuemax="100"/>
+export const Skills = () => {
+	const [itemLeft, setItemLeft] = useState(null)
+	const [itemRight, setItemRight] = useState(null)
+
+	useEffect(() => {
+		(async () => {
+			const res = await fetch('http://localhost:3021/get-one/62c34cede58690199141fe9d');
+			const data = await res.json();
+			const itemLeft = await data.itemLeft;
+			const itemRight = await data.itemRight;
+			setItemLeft(itemLeft);
+			setItemRight(itemRight);
+		})();
+	}, [])
+
+	if (itemLeft === null || itemRight === null) {
+		return <div/>
+	} else {
+		return (
+			<div className="skills-section px-3 px-lg-4">
+				<h2 className="h3 mb-3">Umiejętności</h2>
+				<div className="row">
+					<div className="col-md-6">
+						{
+							itemLeft.map(cvItem => (
+								<div className="mb-2" key={cvItem.name}>
+									<span>{cvItem.name}</span>
+									<div className="progress my-1">
+										<div className="progress-bar bg-primary"
+												 role="progressbar"
+												 data-aos="zoom-in-right"
+												 data-aos-delay={`${cvItem.aosDelay}`}
+												 data-aos-anchor=".skills-section"
+												 style={{width: `${cvItem.value}%`}}
+												 aria-valuenow={`${cvItem.value}`}
+												 aria-valuemin="0" aria-valuemax="100"/>
+									</div>
+								</div>
+							))
+						}
+
 					</div>
-				</div>
-				<div className="mb-2"><span>CSS</span>
-					<div className="progress my-1">
-						<div className="progress-bar bg-primary" role="progressbar" data-aos="zoom-in-right"
-								 data-aos-delay="200" data-aos-anchor=".skills-section" style={{width: '85%'}} aria-valuenow="85"
-								 aria-valuemin="0" aria-valuemax="100"/>
-					</div>
-				</div>
-				<div className="mb-2"><span>JavaScript</span>
-					<div className="progress my-1">
-						<div className="progress-bar bg-primary" role="progressbar" data-aos="zoom-in-right"
-								 data-aos-delay="300" data-aos-anchor=".skills-section" style={{width: "55%"}} aria-valuenow="45"
-								 aria-valuemin="0" aria-valuemax="100"/>
+					<div className="col-md-6">
+						{
+							itemRight.map(cvItem => (
+								<div className="mb-2" key={cvItem.name}>
+									<span>{cvItem.name}</span>
+									<div className="progress my-1">
+										<div className="progress-bar bg-success"
+												 role="progressbar"
+												 data-aos="zoom-in-right"
+												 data-aos-delay={`${cvItem.aosDelay}`}
+												 data-aos-anchor=".skills-section"
+												 style={{width: `${cvItem.value}%`}}
+												 aria-valuenow={`${cvItem.value}`}
+												 aria-valuemin="0" aria-valuemax="100"/>
+									</div>
+								</div>
+							))
+						}
 					</div>
 				</div>
 			</div>
-			<div className="col-md-6">
-				<div className="mb-2"><span>TypeScript</span>
-					<div className="progress my-1">
-						<div className="progress-bar bg-success" role="progressbar" data-aos="zoom-in-right"
-								 data-aos-delay="400" data-aos-anchor=".skills-section" style={{width: "40%"}} aria-valuenow="30"
-								 aria-valuemin="0" aria-valuemax="100"/>
-					</div>
-				</div>
-				<div className="mb-2"><span>MySQl</span>
-					<div className="progress my-1">
-						<div className="progress-bar bg-success" role="progressbar" data-aos="zoom-in-right"
-								 data-aos-delay="500" data-aos-anchor=".skills-section" style={{width: "75%"}} aria-valuenow="85"
-								 aria-valuemin="0" aria-valuemax="100"/>
-					</div>
-				</div>
-				<div className="mb-2"><span>Adobe Photoshop</span>
-					<div className="progress my-1">
-						<div className="progress-bar bg-success" role="progressbar" data-aos="zoom-in-right"
-								 data-aos-delay="600" data-aos-anchor=".skills-section" style={{width: "65%"}} aria-valuenow="75"
-								 aria-valuemin="0" aria-valuemax="100"/>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-)
+		)
+	}
+}
